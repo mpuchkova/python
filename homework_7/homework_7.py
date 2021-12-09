995696204353f18b85437bb869c9c08bd6e5d64a
mpuchkova <puchkovamm2001@mail.ru>
2021-12-09 19:05:22 +0300

    added new  script

--- homework_7/homework_7.py
+++ homework_7/homework_7.py
@@ -4,18 +4,18 @@ from random import randint
 import deff
 
 
-print("____Ex 3____")
+print("____Ex 4____")
 print("Имя операционной системы: ",os.name)
 print("Имя пользователя, вошедшего в терминал: ",os.getlogin())
 print("Cписок файлов и директорий в папке: ",os.listdir())
 
-print("____Ex 4____")
+print("____Ex 5____")
 print("Массив 3*3: ")
 a=np.array([[randint(-10,10) for i in range(3)] for j in range(3)])
 print(a)
 print("Транспонированный маcсив: ", end='\n')
 print(a.transpose())
 
-print("____Ex 5____")
+print("____Ex 6____")
 print(deff.Fibonachi_7(8))
 print(deff.sum_7(1,2,3))
